715b08976d8cb8f601c2ab259725a97731277e4e
hades102zues <joshua.kar@hotmail.com>
2022-08-28 19:49:35 -0400

    added light properties

--- src/cube_shader.fs
+++ src/cube_shader.fs
@@ -18,19 +18,28 @@ struct Material {
 
 uniform Material material;
 
+struct Light {
+
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+};
+
+uniform Light lightProperties;
+
 void main()
 {
-    vec3 ambient = material.ambient * lightColor; //amount of available light
+    vec3 ambient = material.ambient * lightProperties.ambient; //amount of available light
 
     vec3 normal = normalize(Normal); //translating to world space denormalizes the normal
     vec3 toLight = normalize(lightPos - FragPos); //vector from frag to light 
     float diff = max(dot(normal, toLight), 0.0);
-    vec3 diffuse = lightColor * (diff * material.diffuse);
+    vec3 diffuse = lightProperties.diffuse * (diff * material.diffuse);
 
     vec3 viewDir = normalize(cameraPos - FragPos); //vector from frag to camera
     vec3 reflectRay = reflect(-toLight,Normal); //the reflected light ray that hit the surface
     float shine = pow(max(dot(viewDir, reflectRay), 0.0), material.shinny);
-    vec3 specular = lightColor * (shine * material.specular);
+    vec3 specular = lightProperties.specular * (shine * material.specular);
     
 
     vec3 result = ambient + diffuse + specular;
